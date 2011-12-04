26b52c8564209e4b60c391b2e5c2490f0836d261
Matteo Kloiber <matteo.kloiber@me.com>
2011-12-04 19:13:58 +0100

    Revert b6de2d88697137494cc4088950a6c66a22024060^..HEAD

--- de/matt3o/bruchrechner/Fraction.java
+++ de/matt3o/bruchrechner/Fraction.java
@@ -98,13 +98,6 @@ public class Fraction {
 		throw new MathException( "Die beiden Nenner müssen gleich sein!" );
 	}
 	
-	public Fraction multiplicat( Fraction multiplicand ) throws FractionException{
-		int numerator = multiplicand.getNumerator() * this.getNumerator();
-		int denominator = multiplicand.getDenominator() * this.getDenominator();
-		
-		return new Fraction( numerator, denominator ).trim();
-	}
-	
 	@Override
 	public String toString(){
 		return "Fraction: [" + numerator + "/" + denominator + "]";
